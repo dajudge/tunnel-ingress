435371a3e95071f92bed38dea8d63365f193f0ef
Rob Lang <dev@roblang.de>
2024-01-06 23:00:24 +0100

    fixes wireguard-config issue on container-restart after script-error

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,8 +12,6 @@ mkdir -p /root/.ssh/
 cp /ssh-key/* /root/.ssh/
 chmod 0600 /root/.ssh -R
 
-POD_IFACE="eth0"
-POD_IP="$(ip -4 addr show "${POD_IFACE}" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
 
 rm -rf /tmp/wg/
 mkdir -p /tmp/wg/
@@ -31,12 +29,21 @@ remote "PORTS=\"${PORTS}\" INTERNAL_PRIVATE_IP=\"${INTERNAL_PRIVATE_IP}\" EXTERN
 EXTERNAL_PUBKEY="$(remote "cat /tmp/wg/public-key")"
 
 echo "Configuring internal wireguard..."
-ip link add dev wg0 type wireguard
-ip address add dev wg0 "${INTERNAL_PRIVATE_IP}/24"
+if [ ! -f "/tmp/internal_wireguard_configured" ]; then
+  echo "Creating wireguard interface..."
+  ip link add dev wg0 type wireguard
+  ip address add dev wg0 "${INTERNAL_PRIVATE_IP}/24"
+  touch /tmp/internal_wireguard_configured
+else
+  echo "Wireguard interface already existing"
+fi
 wg set wg0 listen-port 51820
 wg set wg0 private-key /tmp/wg/private-key
 wg set wg0 peer "$EXTERNAL_PUBKEY" allowed-ips "${EXTERNAL_PRIVATE_IP}/32" endpoint "${HOST}:51820" persistent-keepalive 5
 ip link set up dev wg0
+
+POD_IFACE="eth0"
+POD_IP="$(ip -4 addr show "${POD_IFACE}" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
 
 echo "Configuring internal iptables..."
 for PORT in $(echo "${PORTS}" | tr ',' '\n'); do
@@ -49,4 +56,4 @@ for PORT in $(echo "${PORTS}" | tr ',' '\n'); do
   iptables -t nat -A POSTROUTING -p ${PROTOCOL} -d "${IP_OUTPUT}" --dport "${PORT_OUTPUT}" -j SNAT --to-source "${POD_IP}"
 done
 
-remote "while :; do date; curl '${PING_URL}'; echo ""; sleep 60; done"
+remote "while :; do date; curl '${PING_URL}' || true; echo ""; sleep 60; done"
